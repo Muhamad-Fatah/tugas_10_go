88007966a5afc51d1a3c10dbe4e65bf87ce5f45a
Muhamad Fatah <muhamad.fatah494@gmail.com>
2022-07-08 15:41:27 +0700

    tugas 10 go
    tugas 10 go - niomic

---
+++ tugas10.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Nama Saya Fatah")
+	time.Sleep(time.Second * 5)
+	fmt.Println("Hobi Saya Coding dan Gaming")
+	time.Sleep(time.Second * 5)
+	fmt.Println("Saya lahir tanggal 4 bulan september 2004")
+}
